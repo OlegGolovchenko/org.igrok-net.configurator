90cdbb8c2d8821befd38c259467a507db6a1865b
Oleg Golovchenko <IgRoK_be@hotmail.com>
2023-04-27 13:47:13 +0200

    scaled back build and run files

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp "org.igrok-net.configurator_v004.jar:org.igrok-net.configurator-gui_v005.jar" org.igrok_net.configurator_gui.ConfiguratorGui
+java -cp "org.igrok-net.configurator_v004.jar:org.igrok-net.configurator-gui_v004.jar" org.igrok_net.configurator_gui.ConfiguratorGui
